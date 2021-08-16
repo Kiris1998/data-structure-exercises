0254b08b95ef2da5e8bca8693eb459103404c391
张宇航 <1148441101@qq.com>
2021-08-16 21:08:02 +0800

    有效括号代码“ git commit -m 有效括号代码“

---
+++ valid-parentheses/valid-parentheses.c
@@ -0,0 +1,30 @@
+﻿char pairs(char a)
+{
+	if(a == ')') return '(';
+	if(a == ']') return '[';
+	if(a == '}') return '{';
+	return 0;
+}
+
+bool vilad(char *s)
+{
+	int n = strlen(s);
+	if(n % 2 == 1)
+	{
+		return false;
+	}
+	int skt[n + 1],top = 0;
+	for(int i = 0;i < n;i++)
+	{
+		c = s[i];
+		if(top !=0 && pairs(c) == skt[top - 1])
+		{
+			top--;
+		}
+		else
+		{
+			skt[top++] = c;
+		}
+	}
+	return top == 0;
+}	
