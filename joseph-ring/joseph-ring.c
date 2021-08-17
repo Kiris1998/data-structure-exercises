d40b7aa94d8b052a315e4d4a64ee8b7a32c28386
张宇航 <1148441101@qq.com>
2021-08-17 11:07:30 +0800

    style:变量名称更改

--- joseph-ring/joseph-ring.c
+++ joseph-ring/joseph-ring.c
@@ -1,9 +1,9 @@
 ﻿int josephring(int n,int ,m) %约瑟夫环函数
 {
-	int rem = 0;   %rem是每次挑选出的座位数
+	int remainseat = 0;   %remainseat是每次挑选出的座位数
 	for(int i = 0;i < n;i++)
 	{
-		rem = (rem + m) % i;
+		remainseat = (remainseat + m) % i;
 	}
-	return rem;
+	return remainseat;
 }	
