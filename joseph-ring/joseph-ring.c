2bb1024e5242c2cf910082be60dd609fb2352eff
张宇航 <1148441101@qq.com>
2021-08-17 12:06:36 +0800

    style:细节修改

--- joseph-ring/joseph-ring.c
+++ joseph-ring/joseph-ring.c
@@ -1,7 +1,7 @@
-﻿int josephring(int n,int ,m) %约瑟夫环函数
+﻿int josephring(int n,int ,m) //约瑟夫环函数,n为座位个数，m为相隔多少抽取一个座位
 {
-	int remainSeat = 0;   %remainSeat是每次挑选出的座位数
-	for(int i = 0;i < n;i++)
+	int remainSeat = 0;   //remainSeat是每次挑选出的座位数
+	for(int i = 0;i < n;i++) //每次循环求出抽选的座位号，最后一次循环为最后剩下的座位
 	{
 		remainSeat = (remainSeat + m) % i;
 	}
--- valid-parentheses/valid-parentheses.c
+++ valid-parentheses/valid-parentheses.c
@@ -1,22 +1,25 @@
-﻿char pairs(char a)  %返回右括号对应的左括号，a为输入的括号
+﻿char pairs(char a)//返回右括号对应的左括号，a为输入的括号
 {
-	if(a == ')') return '(';
-	if(a == ']') return '[';
-	if(a == '}') return '{';
-	return 0;
+    switch(a)
+    {
+        case ')':return '(';break;
+        case ']':return '[';break;
+        case '}':return '{';break;
+    }
+    return 0;
 }
 
-bool vilad(char *string) %布尔函数，括号匹配主函数，返回输入括号形式是否正确
+bool vilad(char *string) //布尔函数，括号匹配主函数，返回输入括号形式是否正确
 {
-	int n = strlen(string);  %输入字符串string的有效字符数
-	if(n % 2 == 1)
+	int n = strlen(string);  //输入字符串string的有效字符数
+	if(n % 2 == 1)  //当输入符号数不为偶数时直接返回假值
 	{
 		return false;
 	}
 	int skt[n + 1],top = 0;  
-	for(int i = 0;i < n;i++)
+	for(int i = 0;i < n;i++) //每次循环提取出一个符号，左括号推入堆栈，遇到右括号与堆栈最顶层匹配，相同则消去
 	{
-		c = string[i];  %c为提取出的单个括号
+		c = string[i];  //c为提取出的单个括号
 		if(top !=0 && pairs(c) == skt[top - 1])
 		{
 			top--;
