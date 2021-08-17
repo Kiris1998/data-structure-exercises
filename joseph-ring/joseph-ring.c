794ebed1a1ff0a7a0569dbb624754bcfe32fce50
张宇航 <1148441101@qq.com>
2021-08-17 11:39:21 +0800

    style:变量名使用驼峰命名

--- joseph-ring/joseph-ring.c
+++ joseph-ring/joseph-ring.c
@@ -1,9 +1,9 @@
 ﻿int josephring(int n,int ,m) %约瑟夫环函数
 {
-	int remainseat = 0;   %remainseat是每次挑选出的座位数
+	int remainSeat = 0;   %remainSeat是每次挑选出的座位数
 	for(int i = 0;i < n;i++)
 	{
-		remainseat = (remainseat + m) % i;
+		remainSeat = (remainSeat + m) % i;
 	}
-	return remainseat;
+	return remainSeat;
 }	
