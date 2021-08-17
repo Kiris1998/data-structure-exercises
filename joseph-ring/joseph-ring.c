6fcb820b50d79941ad02057904a6f6b74aa5d98a
张宇航 <1148441101@qq.com>
2021-08-17 14:52:37 +0800

    style：加亿点点细节

--- joseph-ring/joseph-ring.c
+++ joseph-ring/joseph-ring.c
@@ -1,4 +1,5 @@
-﻿int josephring(int n,int ,m) //约瑟夫环函数,n为座位个数，m为相隔多少抽取一个座位
+﻿//约瑟夫环函数,n为座位个数，m为相隔多少抽取一个座位
+int josephring(int n,int ,m) 
 {
 	int remainSeat = 0;   //remainSeat是每次挑选出的座位数
 	for(int i = 0;i < n;i++) //每次循环求出抽选的座位号，最后一次循环为最后剩下的座位
--- valid-parentheses/valid-parentheses.c
+++ valid-parentheses/valid-parentheses.c
@@ -2,31 +2,39 @@
 {
     switch(a)
     {
-        case ')':return '(';break;
-        case ']':return '[';break;
-        case '}':return '{';break;
+        case ')' :
+			return '(';
+			break;
+        case ']' :
+			return '[';
+			break;
+        case '}' :
+			return '{';
+			break;
+		default:
+			printf("输入错误！\n");
     }
-    return 0;
 }
 
 bool vilad(char *string) //布尔函数，括号匹配主函数，返回输入括号形式是否正确
 {
-	int n = strlen(string);  //输入字符串string的有效字符数
-	if(n % 2 == 1)  //当输入符号数不为偶数时直接返回假值
+	int num = strlen(string);  //输入字符串string的有效字符数
+	if(num % 2 == 1)  //当输入符号数不为偶数时直接返回假值
 	{
 		return false;
 	}
-	int skt[n + 1],top = 0;  
-	for(int i = 0;i < n;i++) //每次循环提取出一个符号，左括号推入堆栈，遇到右括号与堆栈最顶层匹配，相同则消去
+	int storehouse[num + 1];
+	int top = 0;  //创建一个堆栈用来存放左括号
+	for(int i = 0;i < num;i++) //每次循环提取出一个符号，左括号推入堆栈，遇到右括号与堆栈最顶层匹配，相同则消去
 	{
 		c = string[i];  //c为提取出的单个括号
-		if(top !=0 && pairs(c) == skt[top - 1])
+		if(top !=0 && pairs(c) == storehouse[top - 1])
 		{
 			top--;
 		}
 		else
 		{
-			skt[top++] = c;
+			storehouse[top++] = c;
 		}
 	}
 	return top == 0;
