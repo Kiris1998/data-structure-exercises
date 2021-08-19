ca380bfab8b31cbaf84f44c112264239340d6ccc
张宇航 <1148441101@qq.com>
2021-08-19 23:22:56 +0800

    style：驼峰命名

---
+++ Palindrome/isPalindrome.c
@@ -0,0 +1,27 @@
+﻿//给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+//说明：本题中，我们将空字符串定义为有效的回文串。
+
+bool isPalindrome(char *s)  //判断是否为回文串，s为需要判断的字符串
+{
+	int num = strlen(s);  //获得字符串大小
+	int left = 0;  //左指针
+	int right = num - 1;  //右指针
+	while(left < right)  //左右指针对比
+	{
+		while(left < right && !isalnum(s[left]))  //当为非字母或数字时，指针加一
+		{
+			++left;
+		}
+		while(left < right && !isalnum(s[right]))  //当为非字母或数字时，指针减一
+		{
+			--right;
+		}
+		if(tolower(s[left]) != tolower(s[right]))  //不相等时，输出为非回文串
+		{
+			return false;
+		}
+		++left;
+		--right;
+	}
+	return true;
+}
